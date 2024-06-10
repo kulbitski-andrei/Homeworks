8a54cffc6caad202bd1afec1936bdda18b31e98e
Andrei Kulbitski <andrei.kulbitski@gmail.com>
2024-06-10 16:37:37 +0200

    Splitted HWs in 3 files. Fixed HW 10.1

---
+++ homework_10.1.py
@@ -0,0 +1,29 @@
+"""HOMEWORK #10.1"""
+
+
+# 1. Положительные аргументы функции¶
+
+def validate_arguments(func):
+    """wrapper that checks if given function arguments
+    are positive numbers"""
+
+    def wrapper(*args):
+
+        for element in args:
+            if element < 0:
+                raise ValueError
+        return func(args)[0]
+
+    return wrapper
+
+
+@validate_arguments
+def funny_values(*args):
+    """This function just returns values that were provided as an arguments"""
+    return args
+
+
+# TEST CASES
+assert funny_values(100, 200, 300) == (100, 200, 300)
+assert funny_values(5, 0) == ValueError
+assert funny_values(-1) == ValueError
