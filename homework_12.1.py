78e3ff92ca43e8d1a6b49d4bbff52543e0af1b01
Andrei Kulbitski <andrei.kulbitski@gmail.com>
2024-06-14 11:08:53 +0200

    HW 12.1 done, created HW 12.2

--- homework_12.1.py
+++ homework_12.1.py
@@ -1,23 +1,34 @@
-"""HOMEWORK 12.1: CARDS"""
-
 import random
 
+class Card:
+    """Provides a storage for list of card numbers (values)
+     and card suits """
 
-class CardDeckClass:
+    number_list = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
+                        "Jack", "Queen", "King", "Ace"]
+    mast_list = ["Diamonds", "Hearts", "Clubs", "Spades"]
+
+    def __init__(self, number, mast):
+        self.number = number
+        self.mast = mast
+
+
+class CardsDeck:
     """Simple and nice deckbuilder for home usage.
     Creates a new deck exemplar on initialization.
     Can shuffle deck and return chosen card to a user"""
 
-    def __init__(self):
+    def __init__(self, number_list_from_card, mast_list_from_card):
 
         self.deck_list = []
-        self.number_list = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
-                            "Jack", "Queen", "King", "Ace", "Joker"]
-        self.mast_list = ["Diamonds", "Hearts", "Clubs", "Spades"]
 
-        for number in self.number_list:
-            for mast in self.mast_list:
-                self.deck_list.append(number + " of " + mast)
+        for _number in number_list_from_card:
+            for _mast in mast_list_from_card:
+                exemplar_card = Card(_number, _mast)
+                self.deck_list.append(exemplar_card)
+        self.deck_list.append(Card("Joker", "Reds"))
+        self.deck_list.append(Card("Joker", "Blacks"))
+        print(self.deck_list)
 
     def deck_shuffle(self):
         """Shuffles the deck by randomizing elements in the list"""
@@ -30,10 +41,11 @@ class CardDeckClass:
         return self.deck_list[index_number]
 
 
-card_deck_exemplar = CardDeckClass()
-card_deck_exemplar.deck_shuffle()
-card_selected = int(input("There are 56 cards in the deck. "
+exemplar_card_deck = CardsDeck(Card.number_list, Card.mast_list)
+exemplar_card_deck.deck_shuffle()
+
+card_selected_index = int(input("There are 54 cards in the deck. "
                           "Select the sequence number of the card "
                           "you want to reveal... ")) - 1
-card = card_deck_exemplar.deck_get(card_selected)
-print(f"The card you have chosen is a {card}")
+card_selected_instance = exemplar_card_deck.deck_get(card_selected_index)
+print(f"The card you have chosen is a {card_selected_instance.number} of {card_selected_instance.mast}!")
