d3e9c92bc0af70b9d05545c619838af04ec1ac8a
Andrei Kulbitski <andrei.kulbitski@gmail.com>
2024-07-19 08:52:04 +0200

    import logging fixed in library_module.py and homework_20_2.py

--- homework_20_2.py
+++ homework_20_2.py
@@ -1,9 +1,9 @@
 """HOMEWORK 20_2"""
 
-import logging
 import unittest
 import random
 import string
+from log_dir import log_setup
 from modules_for_testing import library_module
 
 
@@ -17,28 +17,18 @@ def generate_random_book():
     return [rnd_book_title, rnd_author_name, rnd_page_number, rnd_isbn]
 
 
-formatter = logging.Formatter(
-    '\n%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-
-logger = logging.getLogger(__name__)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-
-
 class UserTakeABook(unittest.TestCase):
     """This class tests scenarios where a user takes a book."""
 
     @classmethod
     def setUpClass(cls):
-        logger.info("Setting up the test class %s", cls.__name__)
+        log_setup.logger.info("Setting up the test class %s", cls.__name__)
         cls.book_1 = library_module.Book(*generate_random_book())
         cls.reader_1 = library_module.Reader("Reader_1")
         cls.reader_2 = library_module.Reader("Reader_2")
 
     def setUp(self):
-        logger.info("Setting up the test method for %s", self)
+        log_setup.logger.info("Setting up the test method for %s", self)
         self.book_1.status = "Available"
         self.book_1.taken_by = None
         self.book_1.reserved_by = None
@@ -46,34 +36,34 @@ class UserTakeABook(unittest.TestCase):
     def test_user_take_unreserved_book(self):
         """This test checks if a user can take a book
         that is not reserved and not taken by other users."""
-        logger.info("Testing if user can take an unreserved book")
+        log_setup.logger.info("Testing if user can take an unreserved book")
         self.assertTrue(self.reader_1.take_book(self.book_1))
-        logger.info("Test completed")
+        log_setup.logger.info("Test completed")
 
     def test_user_takes_book_when_its_already_taken(self):
         """This test checks if a user can take a book
         that is already taken by another user."""
-        logger.info("Testing if user can't take a book "
-                    "that is already taken by another user")
+        log_setup.logger.info("Testing if user can't take a book "
+                              "that is already taken by another user")
         self.reader_1.take_book(self.book_1)
         self.assertFalse(self.reader_2.take_book(self.book_1))
-        logger.info("Test completed")
+        log_setup.logger.info("Test completed")
 
     def test_take_self_reserved_book(self):
         """This test checks if a user can take a book
         that he/she has reserved."""
-        logger.info("Testing if user can take a book he/she reserved")
+        log_setup.logger.info("Testing if user can take a book he/she reserved")
         self.reader_2.reserve_book(self.book_1)
         self.assertTrue(self.reader_2.take_book(self.book_1))
-        logger.info("Test completed")
+        log_setup.logger.info("Test completed")
 
     def test_take_another_user_reserved_book(self):
         """This test checks if a user can take a book
         that is reserved by another user."""
-        logger.info("Testing if user can't take a book another user reserved")
+        log_setup.logger.info("Testing if user can't take a book another user reserved")
         self.reader_1.reserve_book(self.book_1)
         self.assertFalse(self.reader_2.take_book(self.book_1))
-        logger.info("Test completed")
+        log_setup.logger.info("Test completed")
 
 
 class UserReturnsABook(unittest.TestCase):
@@ -81,13 +71,13 @@ class UserReturnsABook(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        logger.info("Setting up the test class %s", cls.__name__)
+        log_setup.logger.info("Setting up the test class %s", cls.__name__)
         cls.book_2 = library_module.Book(*generate_random_book())
         cls.reader_3 = library_module.Reader("Reader_3")
         cls.reader_4 = library_module.Reader("Reader_4")
 
     def setUp(self):
-        logger.info("Setting up the test method for %s", self)
+        log_setup.logger.info("Setting up the test method for %s", self)
         self.book_2.status = "Available"
         self.book_2.taken_by = None
         self.book_2.reserved_by = None
@@ -95,25 +85,25 @@ class UserReturnsABook(unittest.TestCase):
     def test_user_returns_a_book_he_have(self):
         """This test checks if a user can return a book
         that was previously taken by that user."""
-        logger.info("Testing if user can take an unreserved book")
+        log_setup.logger.info("Testing if user can take an unreserved book")
         self.reader_3.take_book(self.book_2)
         self.assertTrue(self.reader_3.return_book(self.book_2))
-        logger.info("Test completed")
+        log_setup.logger.info("Test completed")
 
     def test_user_returns_a_book_he_doesnt_have(self):
         """This test checks if a user can return a book
         that he/she has not taken."""
-        logger.info("Testing if user can return book he doesn't have")
+        log_setup.logger.info("Testing if user can return book he doesn't have")
         self.assertFalse(self.reader_3.return_book(self.book_2))
-        logger.info("Test completed")
+        log_setup.logger.info("Test completed")
 
     def test_user_returns_a_book_taken_by_another_user(self):
         """This test checks if a user can return a book
         that was taken by another user."""
-        logger.info("Testing if user can return book taken by other user")
+        log_setup.logger.info("Testing if user can return book taken by other user")
         self.reader_4.take_book(self.book_2)
         self.assertFalse(self.reader_3.return_book(self.book_2))
-        logger.info("Test completed")
+        log_setup.logger.info("Test completed")
 
 
 class UserReservesABook(unittest.TestCase):
@@ -121,13 +111,13 @@ class UserReservesABook(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        logger.info("Setting up the test class %s", cls.__name__)
+        log_setup.logger.info("Setting up the test class %s", cls.__name__)
         cls.book_3 = library_module.Book(*generate_random_book())
         cls.reader_5 = library_module.Reader("Reader_5")
         cls.reader_6 = library_module.Reader("Reader_6")
 
     def setUp(self):
-        logger.info("Setting up the test method for %s", self)
+        log_setup.logger.info("Setting up the test method for %s", self)
         self.book_3.status = "Available"
         self.book_3.taken_by = None
         self.book_3.reserved_by = None
@@ -135,14 +125,14 @@ class UserReservesABook(unittest.TestCase):
     def test_user_reserves_a_book_not_reserved(self):
         """This test checks if a user can reserve a book
         that is not reserved by anyone."""
-        logger.info("Testing if user can reserve unreserved book")
+        log_setup.logger.info("Testing if user can reserve unreserved book")
         self.assertTrue(self.reader_5.reserve_book(self.book_3))
-        logger.info("Test completed")
+        log_setup.logger.info("Test completed")
 
     def test_user_reserves_a_book_reserved_by_other_user(self):
         """This test checks if a user can reserve a book
         that is reserved by another user."""
-        logger.info("Testing if user can't reserve reserved book")
+        log_setup.logger.info("Testing if user can't reserve reserved book")
         self.reader_5.reserve_book(self.book_3)
         self.assertFalse(self.reader_6.reserve_book(self.book_3))
-        logger.info("Test completed")
+        log_setup.logger.info("Test completed")
--- modules_for_testing/library_module.py
+++ modules_for_testing/library_module.py
@@ -1,9 +1,8 @@
 """LIBRARY MODULE"""
 
+from log_dir import log_setup
 
-# 1.  Библиотека¶
 
-#
 class Book:
     """Books in the library are described in this class:
     Title, Author, Pages, ISBN and current status:
@@ -32,25 +31,29 @@ class Reader:
     def __init__(self, name):
 
         self.name = name
-        # print(f"В библиотеку записался новый читатель: {self.name}")
+        log_setup.logger.info("В библиотеку записался новый читатель: %s", self.name)
 
     def reserve_book(self, book):
         """Reserving a book to a particular reader
         prevents reserving/taking this book by other reader"""
 
         if book.status == "Reserved" and book.reserved_by != self.name:
-            # print(f"Невозможно зарезервировать книгу '{book.title}' "
-            #       f"для {self.name} - она уже зарезервирована "
-            #       f"другим читателем!")
+            log_setup.logger.info(
+                "Невозможно зарезервировать книгу "
+                "'%s' для %s - она уже зарезервирована "
+                "другим читателем!", book.title, self.name)
             return False
         if book.status == "Taken":
-            # print(f"Невозможно зарезервировать книгу '{book.title}' "
-            #       f"для {self.name} - ее уже кто-то читает!")
+            log_setup.logger.info(log_setup.logger.info(
+                "Невозможно зарезервировать книгу "
+                "'%s' для %s - ее уже кто-то читает!",
+                book.title, self.name))
             return False
         book.status = "Reserved"
         book.reserved_by = self.name
-        # print(f"Книга '{book.title}' была "
-        #       f"зарезервирована читателем {self.name}")
+        log_setup.logger.info(
+            "Книга '%s' была зарезервирована читателем %s",
+            book.title, self.name)
         return True
 
     def take_book(self, book):
@@ -58,20 +61,27 @@ class Reader:
         prevents reserving/taking this book by anyone"""
 
         if book.status == "Reserved" and book.reserved_by != self.name:
-            # print(f"Невозможно взять книгу '{book.title}' "
-            #       f"для {self.name} - она уже зарезервирована "
-            #       f"другим читателем!")
+            log_setup.logger.info(
+                "Невозможно взять книгу '%s' для %s - "
+                "она уже зарезервирована другим читателем!",
+                book.title, self.name)
+
             return False
         if book.status == "Taken":
-            # print(f"Невозможно взять книгу '{book.title}' "
-            #       f"для {self.name} - ее уже взял кто-то другой!")
+            log_setup.logger.info(
+                "Невозможно взять книгу '%s' для %s - "
+                "ее уже взял кто-то другой!",
+                book.title, self.name)
+
             return False
 
         book.status = "Taken"
         book.taken_by = self.name
         book.reserved_by = None
-        # print(f"Книга '{book.title}' была "
-        #       f"взята из библиотеки читателем {self.name}")
+        log_setup.logger.info(
+            "Книга '%s' была взята из библиотеки читателем %s",
+            book.title, self.name)
+
         return True
 
     def return_book(self, book):
@@ -79,29 +89,30 @@ class Reader:
         to be reserved/taken by any reader again."""
 
         if book.taken_by != self.name:
-            # print("Нельзя вернуть то, чего у вас нет!")
+            log_setup.logger.info("Нельзя вернуть то, чего у вас нет!")
             return False
         book.status = "Available"
         book.taken_by = None
         book.reserved_by = None
-        # print(f"Книга '{book.title}' была возвращена в библиотеку")
+        log_setup.logger.info(f"Книга '%s' была возвращена в библиотеку", {book.title})
         return True
 
 
-# book_1 = Book("Foundation", "Isaac Asimov", 725, 220180)
-# book_2 = Book("Solaris", "Stanislaw Lem", 344, 531240)
-#
-# reader_finn = Reader("Finn the Human")
-# reader_jake = Reader("Jake the Dog")
-#
-# reader_jake.reserve_book(book_1)
-# assert not reader_finn.reserve_book(book_1)
-# assert not reader_finn.take_book(book_1)
-#
-# reader_jake.take_book(book_1)
-# assert not reader_finn.reserve_book(book_1)
-# assert not reader_finn.take_book(book_1)
-#
-# reader_jake.return_book(book_1)
-# assert reader_finn.reserve_book(book_1)
-# assert reader_finn.take_book(book_1)
+if __name__ == "__main__":
+    book_1 = Book("Foundation", "Isaac Asimov", 725, 220180)
+    book_2 = Book("Solaris", "Stanislaw Lem", 344, 531240)
+
+    reader_finn = Reader("Finn the Human")
+    reader_jake = Reader("Jake the Dog")
+
+    reader_jake.reserve_book(book_1)
+    assert not reader_finn.reserve_book(book_1)
+    assert not reader_finn.take_book(book_1)
+
+    reader_jake.take_book(book_1)
+    assert not reader_finn.reserve_book(book_1)
+    assert not reader_finn.take_book(book_1)
+
+    reader_jake.return_book(book_1)
+    assert reader_finn.reserve_book(book_1)
+    assert reader_finn.take_book(book_1)
