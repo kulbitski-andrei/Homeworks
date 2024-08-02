7d93a8976b4f99fc5457b2fdc1487c7722de615b
Andrei Kulbitski <andrei.kulbitski@gmail.com>
2024-08-02 23:55:30 +0200

    added assertions for test 1 and test 2

--- homework_24/tests/tests.py
+++ homework_24/tests/tests.py
@@ -33,11 +33,12 @@ def test_create_new_contact(browser):
     page_object.click_add_contact()
     time.sleep(1)
     page_object = AddContactPage(browser)
-    page_object.complete_add_new_contact(edit_first_name, edit_last_name,
-                                         edit_birthdate, edit_email_address,
-                                         edit_phone, edit_street1, edit_street2,
-                                         edit_city, edit_state_province,
-                                         edit_postal_code, edit_country)
+
+    page_object.complete_add_new_contact(first_name, last_name, birthdate,
+                                         email_address, phone, street1, street2,
+                                         city, state_province, postal_code, country)
+    page_object = ContactListPage(browser)
+    assert page_object.locate_contact_row() is not None
     logger.info("TEST 1: Executed")
 
 
@@ -54,9 +55,25 @@ def test_edit_contact(browser):
     page_object.click_edit_contact()
     time.sleep(1)
     page_object = EditContactPage(browser)
-    page_object.complete_edit_contact(first_name, last_name, birthdate,
-                                      email_address, phone, street1, street2,
-                                      city, state_province, postal_code, country)
+    page_object.complete_edit_contact(edit_first_name, edit_last_name,
+                                      edit_birthdate, edit_email_address,
+                                      edit_phone, edit_street1, edit_street2,
+                                      edit_city, edit_state_province,
+                                      edit_postal_code, edit_country)
+    page_object = ContactDetailsPage(browser)
+    time.sleep(1)  # This one is really needed!
+    assert page_object.locate_first_name().text == "RICARDO"
+    assert page_object.locate_last_name().text == "DIAZ"
+    assert page_object.locate_birthdate().text == "1999-12-12"
+    assert page_object.locate_email_address().text == "hello@world.com"
+    assert page_object.locate_phone().text == "0987654321"
+    assert page_object.locate_street1().text == "BOOLEVARD OF BROKEN DREAMS"
+    assert page_object.locate_street2().text == "80-180"
+    assert page_object.locate_city().text == "Kyoto"
+    assert page_object.locate_state_province().text == "AR"
+    assert page_object.locate_postal_code().text == "200205"
+    assert page_object.locate_country().text == "FRANCE"
+    time.sleep(5)
     logger.info("TEST 2: Executed")
 
 
