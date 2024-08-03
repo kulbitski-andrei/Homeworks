753da791c6c87e813149f56084f51330c6c5fa03
Andrei Kulbitski <andrei.kulbitski@gmail.com>
2024-08-03 10:21:03 +0200

    wildcard import fixed

--- homework_24/tests/tests.py
+++ homework_24/tests/tests.py
@@ -1,16 +1,17 @@
 """PYTEST TEST CASES"""
 
+import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
 from homework_24.pages.login_page import LoginPage
 from homework_24.pages.contact_list_page import ContactListPage
 from homework_24.pages.contact_details_page import ContactDetailsPage
 from homework_24.pages.edit_contact_page import EditContactPage
 from homework_24.pages.add_contact_page import AddContactPage
-from homework_24.test_data.constants import *
+import homework_24.test_data.constants as const
 from log_dir.log_setup import logger
+
 
 @pytest.fixture
 def browser():
@@ -22,7 +23,7 @@ def browser():
     chrome_options.add_argument("--disable-search-engine-choice-screen")
     chrome_browser = webdriver.Chrome(options=chrome_options)
     chrome_browser.implicitly_wait(5)
-    chrome_browser.get(URL)
+    chrome_browser.get(const.URL)
     yield chrome_browser
     chrome_browser.quit()
 
@@ -34,16 +35,16 @@ def test_create_new_contact(browser):
     """
     logger.info("TEST 1: CREATE NEW CONTACT. Executing...")
     page_object = LoginPage(browser)
-    page_object.complete_login(EMAIL, PASSWORD)
+    page_object.complete_login(const.EMAIL, const.PASSWORD)
     time.sleep(1)
     page_object = ContactListPage(browser)
     page_object.click_add_contact()
     time.sleep(1)
     page_object = AddContactPage(browser)
 
-    page_object.complete_add_new_contact(FIRST_NAME, LAST_NAME, BIRTHDATE,
-                                         EMAIL_ADDRESS, PHONE, STREET1, STREET2,
-                                         CITY, STATE_PROVINCE, POSTAL_CODE, COUNTRY)
+    page_object.complete_add_new_contact(const.FIRST_NAME, const.LAST_NAME, const.BIRTHDATE,
+                                         const.EMAIL_ADDRESS, const.PHONE, const.STREET1, const.STREET2,
+                                         const.CITY, const.STATE_PROVINCE, const.POSTAL_CODE, const.COUNTRY)
     page_object = ContactListPage(browser)
     assert page_object.locate_contact_row() is not None
     logger.info("TEST 1: Executed")
@@ -56,7 +57,7 @@ def test_edit_contact(browser):
     """
     logger.info("TEST 2: EDIT CONTACT. Executing...")
     page_object = LoginPage(browser)
-    page_object.complete_login(EMAIL, PASSWORD)
+    page_object.complete_login(const.EMAIL, const.PASSWORD)
     time.sleep(1)
     page_object = ContactListPage(browser)
     page_object.open_contact()
@@ -65,11 +66,11 @@ def test_edit_contact(browser):
     page_object.click_edit_contact()
     time.sleep(1)
     page_object = EditContactPage(browser)
-    page_object.complete_edit_contact(EDIT_FIRST_NAME, EDIT_LAST_NAME,
-                                      EDIT_BIRTHDATE, EDIT_EMAIL_ADDRESS,
-                                      EDIT_PHONE, EDIT_STREET1, EDIT_STREET2,
-                                      EDIT_CITY, EDIT_STATE_PROVINCE,
-                                      EDIT_POSTAL_CODE, EDIT_COUNTRY)
+    page_object.complete_edit_contact(const.EDIT_FIRST_NAME, const.EDIT_LAST_NAME,
+                                      const.EDIT_BIRTHDATE, const.EDIT_EMAIL_ADDRESS,
+                                      const.EDIT_PHONE, const.EDIT_STREET1, const.EDIT_STREET2,
+                                      const.EDIT_CITY, const.EDIT_STATE_PROVINCE,
+                                      const.EDIT_POSTAL_CODE, const.EDIT_COUNTRY)
     page_object = ContactDetailsPage(browser)
     time.sleep(1)  # This one is really needed!
     assert page_object.locate_first_name().text == "RICARDO"
@@ -94,7 +95,7 @@ def test_delete_contact(browser):
     """
     logger.info("TEST 3: DELETE CONTACT. Executing...")
     page_object = LoginPage(browser)
-    page_object.complete_login(EMAIL, PASSWORD)
+    page_object.complete_login(const.EMAIL, const.PASSWORD)
     time.sleep(1)
     page_object = ContactListPage(browser)
     page_object.open_contact()
