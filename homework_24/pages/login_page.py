3a3af62291a5ea91bc5e98bf0fbe101c41206709
Andrei Kulbitski <andrei.kulbitski@gmail.com>
2024-08-03 08:31:29 +0200

    added docstrings for: login_page.py

--- homework_24/pages/login_page.py
+++ homework_24/pages/login_page.py
@@ -10,21 +10,36 @@ submit_button = (By.ID, "submit")
 
 class LoginPage(BasePage):
     def __init__(self, driver):
+        """
+        Initialize the LoginPage with a web driver.
+        """
         super().__init__(driver)
 
     def enter_email(self, email):
+        """
+        Enter the email address into the email field.
+        """
         email_input = self.find_element(email_field)
         email_input.send_keys(email)
 
     def enter_password(self, password):
+        """
+        Enter the password into the password field.
+        """
         password_input = self.find_element(password_field)
         password_input.send_keys(password)
 
     def click_submit(self):
+        """
+        Click the submit button to log in.
+        """
         submit_element = self.find_element(submit_button)
         submit_element.click()
 
     def complete_login(self, email, password):
+        """
+        Complete the login process by entering the email and password and submitting the form.
+        """
         self.enter_email(email)
         self.enter_password(password)
         self.click_submit()
