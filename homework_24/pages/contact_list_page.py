dc332d11ba0e9f3eccbaf3814485e7d522ce5098
Andrei Kulbitski <andrei.kulbitski@gmail.com>
2024-08-03 08:28:13 +0200

    added docstrings for: contact_list_page.py

--- homework_24/pages/contact_list_page.py
+++ homework_24/pages/contact_list_page.py
@@ -1,20 +1,35 @@
+"""CONTACT LIST PAGE"""
+
 from homework_24.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 add_contact_button = (By.ID, "add-contact")
 contact_row = (By.XPATH, "//tr[@class=\"contactTableBodyRow\"][1]")
 
+
 class ContactListPage(BasePage):
     def __init__(self, driver):
+        """
+        Initialize the ContactListPage with a web driver.
+        """
         super().__init__(driver)
 
     def click_add_contact(self):
+        """
+        Click the add contact button to navigate to the add contact page.
+        """
         add_contact_element = self.find_element(add_contact_button)
         add_contact_element.click()
 
     def open_contact(self):
+        """
+        Click the first contact row to open the contact details.
+        """
         open_contact = self.find_element(contact_row)
         open_contact.click()
 
     def locate_contact_row(self):
+        """
+        Locate the first contact row in the contact list.
+        """
         return self.find_element(contact_row)
