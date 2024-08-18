de6671e476ac8260f46a59f74f0a224e3463ae4e
Andrei Kulbitski <andrei.kulbitski@gmail.com>
2024-08-18 19:37:08 +0200

    Linters satisfied

--- homework_29/flask_app/app.py
+++ homework_29/flask_app/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+"""APP.PY"""
+
 import random
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -13,6 +15,9 @@ images = [
 
 @app.route('/')
 def index():
+    """
+    Creates a render template with random cat gif
+    """
     url = random.choice(images)
     return render_template('index.html', url=url)
 
